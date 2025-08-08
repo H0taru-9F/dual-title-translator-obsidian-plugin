aec55e5a8b02ade02ef888ccd002f57e9d404d03
Vasyl <feniak2003@gmail.com>
2025-08-08 13:26:46 +0300

    Refactor titleRename function and update settings to handle translated file names

--- main.ts
+++ main.ts
@@ -1,18 +1,25 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting,} from 'obsidian';
-import titleRename from "./src/titleRename";
+import {App, Editor, MarkdownView, Notice, Plugin, PluginSettingTab, Setting, TFile,} from 'obsidian';
+import {titleRename} from "./src/titleRename";
 // Remember to rename these classes and interfaces!
 
+interface TranslatedFiles {
+	path: string;
+	name: string;
+}
+
 export interface DualTitleTranslatorSettings {
-	mySetting: string;
+	translatedFiles: TranslatedFiles[];
 	api: string;
 	separator: string;
 }
 
 const DEFAULT_SETTINGS: DualTitleTranslatorSettings = {
-	mySetting: 'default',
+	translatedFiles: [],
 	api: "",
 	separator : '⇋',
 }
+
+
 
 export default class DualTitleTranslator extends Plugin {
 	settings: DualTitleTranslatorSettings;
@@ -20,28 +27,16 @@ export default class DualTitleTranslator extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('circle', 'Sample Plugin', (evt: MouseEvent) => {
-			titleRename(this.app, this.settings)
-			// titleRename()
-			// Called when the user clicks the icon.=
-			// new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.registerEvent(
+			this.app.vault.on('rename', (file: TFile, oldPath:string) => {
+				if (file.extension === 'md') {
+					if (file.path === this.app.workspace.getActiveFile()?.path) {
+						titleRename(this.app, this.settings, file, oldPath)
+					}
+				}
+			})
+		);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		// const statusBarItemEl = this.addStatusBarItem();
-		// statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
 			id: 'sample-editor-command',
@@ -51,34 +46,15 @@ export default class DualTitleTranslator extends Plugin {
 				editor.replaceSelection('Sample Editor Command');
 			}
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+		// 	console.log('click', evt);
+		// });
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
@@ -97,22 +73,6 @@ export default class DualTitleTranslator extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
 class SampleSettingTab extends PluginSettingTab {
 	plugin: DualTitleTranslator;
 
@@ -125,17 +85,6 @@ class SampleSettingTab extends PluginSettingTab {
 		const {containerEl} = this;
 
 		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 
 		new Setting(containerEl)
 			.setName('API Key')
--- src/titleRename.ts
+++ src/titleRename.ts
@@ -1,31 +1,46 @@
-import {App, Notice} from "obsidian";
+import {App, Notice, TFile} from "obsidian";
 import {DualTitleTranslatorSettings} from "../main";
+import deeplTranslate from "./deeplTranslate";
 
-export default async function titleRename(app:App, settings:DualTitleTranslatorSettings) {
+export async function titleRename(app: App, settings: DualTitleTranslatorSettings, file:TFile, oldPath: string) {
 
-	const separator = ` ${settings.separator} `;
+	const separator = `${settings.separator}`;
 
 	const activeFile = app.workspace.getActiveFile();
 
+	const path = window.require("path");
+	const oldName = path.basename(oldPath, `.${file.extension}`);
+	const newName = file.basename
 
-	if (activeFile && activeFile.parent) {
+	if (!activeFile?.basename.includes(separator)) {
+		if (activeFile && activeFile.parent) {
 
-		const fileName = `${activeFile.basename}`;
+			const fileName = `${activeFile.basename}`;
 
-		// const translatedName = await deeplTranslate(fileName, 'EN', settings.api);
-		const translatedName = "placeholder"; // Replace with actual translation logic
+			// const translatedName = await deeplTranslate(fileName, 'EN', settings.api);
+			const translatedName = "placeholder"; // Replace with actual translation logic
 
-		const extension = `.${activeFile.extension}`
-		const newName = `${fileName}${separator}${translatedName}${extension}`;
+			const extension = `.${activeFile.extension}`
+			const newName = `${fileName} ${separator} ${translatedName}${extension}`;
 
-		try {
-			await app.fileManager.renameFile(activeFile, `${activeFile.parent.path}/${newName}`);
-			console.log(activeFile)
-		} catch (error) {
-			console.error('Error renaming file:', error);
+			try {
+				await app.fileManager.renameFile(activeFile, `${activeFile.parent.path}/${newName}`);
+				console.log(activeFile)
+			} catch (error) {
+				console.error('Error renaming file:', error);
+			}
+		}else {
+			new Notice('Something is wrong!');
 		}
-	}else {
-		new Notice('Something is wrong!');
+
+	}else if (oldName !== newName) {
+		console.log("name changed", oldName, newName);
+		const nameBeforeSeparator = newName.split(separator)[0];
+		console.log("nameBeforeSeparator",nameBeforeSeparator);
+
+	} else {
+		new Notice('File already has a translated title!');
+		return;
 	}
 
 }
